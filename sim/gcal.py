ed8338a96b994dc1f5b37295be608f7cc168698d
agouliel <agouliel@gmail.com>
2026-04-28 10:31:55 +0300

    dont use token file

--- sim/gcal.py
+++ sim/gcal.py
@@ -1,16 +1,12 @@
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from .models import User
+import ast
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def get_service(user_email):
     user = User.objects.get(email=user_email)
-
-    token_file = 'token.json'
-    with open(token_file, 'w') as token:
-        token.write(user.google_calendar_token)
-
-    creds = Credentials.from_authorized_user_file(token_file, SCOPES)
+    creds = Credentials.from_authorized_user_info(ast.literal_eval(user.google_calendar_token), SCOPES)
     service = build('calendar', 'v3', credentials=creds)
     return service
